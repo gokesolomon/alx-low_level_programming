baa67e1b2233418ce3fc5326acf0d55e2984ffce
gokesolomon <gokehsolomon@gmail.com>
2023-02-16 19:06:11 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints an exact line with printf function
- * Return: 0 (success)
+ * main - Entry point
+ *
+ *Return: Always (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("\"with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
