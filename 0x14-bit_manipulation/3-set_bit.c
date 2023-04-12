0be31f12e592cb59cb716fcaadad36d30f0c3c5d
gokesolomon <gokehsolomon@gmail.com>
2023-04-12 16:11:50 -0700

    3.11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,6 +8,7 @@
  * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
+
 {
 	unsigned long int t;
 
@@ -15,5 +16,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	t = 1 << index;
 	*n = *n | t;
+
+	return (1);
 
 }
