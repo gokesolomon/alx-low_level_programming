7f031397cf12cda935f21e4421f2ababb32bf308
gokesolomon <gokehsolomon@gmail.com>
2023-04-12 00:44:24 -0700

    1.1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,18 +7,10 @@
 
 void print_binary(unsigned long int n)
 {
-	#include "main.h"
-
-/**
- * print_binary - prints the binary equivalent of a decimal number
- * @n: number to print in binary
- */
-void print_binary(unsigned long int n)
-{
 	int i, kalc = 0;
 	unsigned long int pres;
 
-	for (i = 63; i >= 0; i--)
+	for (i = ? ; i >= 0; i--)
 	{
 		pres = n >> i;
 
